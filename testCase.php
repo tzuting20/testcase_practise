f8ec69ab54056158643f002df9b9273cea6d4e33
LucyChen <tzu880501@gmail.com>
2023-02-05 15:31:38 +0800

    Add deposit test

--- testCase.php
+++ testCase.php
@@ -21,16 +21,46 @@ function testNewAct()
     ];
     for ($i = 0; $i < count($testcase); $i++) { 
         echo "testing {$testcase[$i][0]}, {$testcase[$i][1]}: ";
-        if($testcase[$i][2] == true){
-            if(is_int(newAct($testcase[$i][0], $testcase[$i][1])) == $testcase[$i][2]){
+        if ($testcase[$i][2] == true) {
+            if (is_int(newAct($testcase[$i][0], $testcase[$i][1])) == $testcase[$i][2]) {
                 echo "ok";
-            } else{
+            } else {
                 echo "error";
             }
         } else {
-            if(newAct($testcase[$i][0], $testcase[$i][1]) == $testcase[$i][2]) {
+            if (newAct($testcase[$i][0], $testcase[$i][1]) == $testcase[$i][2]) {
                 echo "ok";
-            } else{
+            } else {
+                echo "error";
+            }
+        }
+        echo "<br>";
+    }
+}
+
+function testDeposit() {
+    echo "<br><br>test deposit<br><br>";
+    $testcase = [
+        //測試存款功能
+        [1,1,1],
+        //帳號不存在
+        [99999,1,0],
+        //存款金額
+        [1,0,1],
+        [1,-100,0]
+    ];
+    for ($i = 0; $i < count($testcase); $i++) { 
+        echo "testing {$testcase[$i][0]}, {$testcase[$i][1]}: ";
+        if ($testcase[$i][2] == true) {
+            if (deposit($testcase[$i][0], $testcase[$i][1]) == $testcase[$i][2]) {
+                echo "ok";
+            } else {
+                echo "error";
+            }
+        } else {
+            if (deposit($testcase[$i][0], $testcase[$i][1]) == $testcase[$i][2]) {
+                echo "ok";
+            } else {
                 echo "error";
             }
         }
@@ -41,4 +71,5 @@ function testNewAct()
 ?>
 <?php
     testNewAct();
+    testDeposit();
 ?>
